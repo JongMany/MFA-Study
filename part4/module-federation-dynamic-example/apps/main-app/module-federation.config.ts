4d7932eeedf22cb46513684e597e3c87ee39b4e5
JongMany <blackberry1114@naver.com>
2025-05-04 20:46:39 +0900

    feat: promise based dynamic remotes

--- part4/module-federation-dynamic-example/apps/main-app/module-federation.config.ts
+++ part4/module-federation-dynamic-example/apps/main-app/module-federation.config.ts
@@ -4,7 +4,32 @@ export const mfConfig = {
   name: "main_app",
   filename: "remoteEntry.js",
   remotes: {
-    dynamic: "component_app1@http://localhost:3001/remoteEntry.js",
+    dynamic: `promise new Promise(resolve => {
+      fetch("http://localhost:4000/remote.json")
+        .then((res) => res.json())
+        .then(({remoteUrl, scope}) => {
+          console.log(remoteUrl, scope);
+
+          const script = document.createElement('script')
+          script.src = remoteUrl;
+          script.onload = () => {
+            const proxy = {
+              get: (request) => window[scope].get(request),
+              init: (...arg) => {
+                try {
+                  return window[scope].init(...arg)
+                } catch(e) {
+                  console.log('remote container already initialized')
+                }
+              }
+            }
+            resolve(proxy)
+          }
+
+          document.head.appendChild(script);
+        });
+      })
+    `,
   },
   shared: {
     ...deps,
