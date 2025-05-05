17000559385889eb6f584740f3be5a123563be33
JongMany <blackberry1114@naver.com>
2025-05-05 14:32:54 +0900

    feat(posting): inject 및 routing 기능 추가

---
+++ career-up/apps/posting/src/routes.tsx
@@ -0,0 +1,14 @@
+import { AppRoutingManager } from "@career-up/shell-router";
+import { RouteObject } from "react-router-dom";
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <AppRoutingManager type="app-posting" />,
+    errorElement: <div>App Posting Error</div>,
+    children: [
+      { index: true, element: <div>App Posting Root</div> },
+      { path: "1", element: <div>App Posting Page 1</div> },
+    ],
+  },
+];
