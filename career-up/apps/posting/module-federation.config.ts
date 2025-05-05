1c32a7933d2adbafc84aa6ef9da01edb30a0d069
JongMany <blackberry1114@naver.com>
2025-05-05 14:27:17 +0900

    chore: posting mf-app 설정

---
+++ career-up/apps/posting/module-federation.config.ts
@@ -0,0 +1,24 @@
+const deps = require("./package.json").dependencies;
+export const mfConfig = {
+  name: "posting",
+  exposes: {
+    "./injector": "./src/injector.tsx",
+  },
+  shared: {
+    ...deps,
+    react: {
+      singleton: true,
+      requiredVersion: deps.react,
+    },
+    "react-dom": {
+      singleton: true,
+      requiredVersion: deps["react-dom"],
+    },
+    "@career-up/ui-kit": {
+      singleton: true,
+    },
+    "@career-up/shell-router": {
+      singleton: true,
+    },
+  },
+};
