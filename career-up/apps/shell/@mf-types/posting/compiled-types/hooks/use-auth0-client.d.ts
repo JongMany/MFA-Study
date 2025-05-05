2860b3f41296c93e8071bc6feb0d6c812b381c28
JongMany <blackberry1114@naver.com>
2025-05-05 17:33:52 +0900

    feat(posting): auth 가져오는 코드 추가

---
+++ career-up/apps/shell/@mf-types/posting/compiled-types/hooks/use-auth0-client.d.ts
@@ -0,0 +1,2 @@
+import { Auth0Client } from "@auth0/auth0-spa-js";
+export default function useAuth0Client(): Auth0Client;
---
+++ career-up/apps/shell/@mf-types/posting/compiled-types/pages/page-home.d.ts
@@ -0,0 +1,1 @@
+export default function PageHome(): import("react/jsx-runtime").JSX.Element;
