bdf6e5cdb1f769c2aff404825a88d884847c5d0e
JongMany <blackberry1114@naver.com>
2025-05-05 11:57:07 +0900

    refactor: 폴더 정리

--- career-up/apps/shell/src/App.tsx
+++ career-up/apps/shell/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
+import "@career-up/ui-kit/index.css";
 import { Button } from "@career-up/ui-kit";
 
 const App = () => (
