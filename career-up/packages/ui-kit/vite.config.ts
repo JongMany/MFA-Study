4f5105cef4766bee33d308e02aba8f46462b663c
JongMany <blackberry1114@naver.com>
2025-05-05 11:55:53 +0900

    feat: global style 추가 및 빌드 기능 추가

--- career-up/packages/ui-kit/vite.config.ts
+++ career-up/packages/ui-kit/vite.config.ts
@@ -27,6 +27,12 @@ export default defineConfig({
           react: "React",
           "react-dom": "ReactDOM",
         },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "style.css") {
+            return "index.css";
+          }
+          return assetInfo.name!;
+        },
       },
     },
   },
