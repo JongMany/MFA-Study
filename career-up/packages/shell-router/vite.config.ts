5cc7cd19ea2b8e597c64d9d1c4dbf94f78494c91
JongMany <blackberry1114@naver.com>
2025-05-05 13:43:00 +0900

    chore(shell-router): react-router-dom vite rollup 설정 처리

--- career-up/packages/shell-router/vite.config.ts
+++ career-up/packages/shell-router/vite.config.ts
@@ -19,11 +19,12 @@ export default defineConfig({
       fileName: "index",
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react-router-dom"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          "react-router-dom": "ReactRouterDOM",
         },
       },
     },
