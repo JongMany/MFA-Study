56a146028e086719a67a2a70d5cb7e6d99c818c5
JongMany <blackberry1114@naver.com>
2025-05-06 10:38:20 +0900

    chore(edu): edu mf-app routes 설정 추가

---
+++ career-up/apps/edu/src/routes.tsx
@@ -0,0 +1,24 @@
+// career-up/apps/edu/src/routes.tsx
+
+import React from "react";
+import { type RouteObject } from "react-router-dom";
+import { AppRoutingManager } from "@career-up/shell-router";
+import Auth0ClientProvider from "./providers/auth0-client-provider";
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: (
+      <Auth0ClientProvider>
+        <AppRoutingManager type="app-edu" />
+      </Auth0ClientProvider>
+    ),
+    errorElement: <div>App Edu Error</div>,
+    children: [
+      {
+        index: true,
+        element: <div>edu home</div>,
+      },
+    ],
+  },
+];
